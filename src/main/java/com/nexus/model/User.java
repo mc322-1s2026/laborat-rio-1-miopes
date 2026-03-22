28143c75e09d5521c9c23b2804834d0d26bcdbd0
Juliana Costa Dantas <julianacdantas2006@gmail.com>
2026-03-22 18:02:07 -0300

    implementei a validação de email na classe User

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -8,6 +8,10 @@ public class User {
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
         }
+        String regex = "^[\\w.-]+@[\\w.-]+\\.[a-z]{2,3}$";
+        if (email == null || !email.matches(regex)){
+            throw new IllegalArgumentException("Email inválido: deve seguir o formato usuario@dominio.com");
+        }
         this.username = username;
         this.email = email;
     }
