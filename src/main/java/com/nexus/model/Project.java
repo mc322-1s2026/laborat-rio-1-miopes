74c8f9e0ee910029102b3b01d13cbcb5a16c308a
Juliana Costa Dantas <julianacdantas2006@gmail.com>
2026-03-23 12:46:43 -0300

    fiz a função addTask (de Project) adicionar tarefa

--- src/main/java/com/nexus/model/Project.java
+++ src/main/java/com/nexus/model/Project.java
@@ -26,6 +26,8 @@ public class Project {
         if ((usedBudget + t.getEstimatedEffort()) > totalBudget){
             throw new NexusValidationException("O orçamento do projeto não pode ser ultrapassado");
         }
+
+        this.tasks.add(t);
     }
 
     public String getName() { return name; }
